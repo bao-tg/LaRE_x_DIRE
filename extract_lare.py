f8b2966449682845cfbf1bb4ccbb0dd599a686ed
骆云鹏 <591486669@qq.com>
2024-12-03 15:35:25 +0800

    update

--- extract_lare.py
+++ extract_lare.py
@@ -34,7 +34,6 @@ from diffusers.optimization import get_scheduler
 from diffusers.training_utils import EMAModel
 from diffusers.utils import check_min_version, deprecate, is_wandb_available
 from diffusers.utils.import_utils import is_xformers_available
-from dataset import TxTDataset
 import argparse
 import multiprocessing as mp
 import json
